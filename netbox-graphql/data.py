e752ec9075f30541852283019ec3ddd45f97ff8f
misazr <miodrag.seslija@consulteer.com>
2017-09-04 13:14:30 +0200

    Provider CRUD unit tests

--- netbox-graphql/data.py
+++ netbox-graphql/data.py
@@ -8,3 +8,17 @@ def initialize_circuits():
         slug = 'type1'
     )
     circuit_type.save()
+
+    provider = Provider(
+        id = '111',
+        name = 'Provider 1',
+        slug = 'provider1',
+        asn = '256',
+        account = '12345',
+        portal_url = 'https://www.nine.ch',
+        noc_contact = 'noc_contact',
+        admin_contact = 'admin_contact',
+        comments = 'comments',
+        created = '2015-01-15'
+    )
+    provider.save()
--- netbox-graphql/testing/tests_circuits.py
+++ netbox-graphql/testing/tests_circuits.py
@@ -120,7 +120,7 @@ class CircuitTypeTestCase(TestCase):
         }
         '''
         expected = { 'newCircuitType':
-            {'circuitType': {'id': 'Q2lyY3VpdFR5cGVOb2RlOjE=', 'name': 'Type', 'slug': 'type'}}}
+                         {'circuitType': {'id': 'Q2lyY3VpdFR5cGVOb2RlOjE=', 'name': 'Type', 'slug': 'type'}}}
 
         result = schema.execute(query)
         assert not result.errors
@@ -184,6 +184,116 @@ class CircuitTypeTestCase(TestCase):
         expected = {'deleteCircuitType':
                         {'circuitType': {'id': 'Q2lyY3VpdFR5cGVOb2RlOk5vbmU=', 'name': 'Type 1', 'slug': 'type1'}}}
 
+        result = schema.execute(query)
+        assert not result.errors
+        assert result.data == expected
+
+class ProviderTestCase(TestCase):
+    def test_creating_new_provider(self):
+        query = '''
+        mutation {
+          newProvider(input: {name: "Provider123", slug: "provider123", asn: 256, account: "account",
+          portalUrl: "http://github.com/", nocContact:"noc", comments: "my comment"}) {
+            provider {
+              id
+              slug
+              name
+              asn
+              account
+              portalUrl
+              nocContact
+              comments
+            }
+          }
+        }
+        '''
+        expected = {'newProvider':
+                        {'provider': {'id': 'UHJvdmlkZXJOb2RlOjE=', 'slug': 'provider123', 'name': 'Provider123',
+                                      'asn': 256.0, 'account': 'account', 'portalUrl': 'http://github.com/',
+                                      'nocContact': 'noc', 'comments': 'my comment'}}}
+        result = schema.execute(query)
+        assert not result.errors
+        assert result.data == expected
+
+    def test_correct_fetch_of_provider(self):
+        initialize_circuits()
+        query = '''
+        {
+          providers(id: "Q2lyY3VpdFR5cGVOb2RlOjExMQ==") {
+            edges {
+              node {
+                id
+                slug
+                name
+                asn
+                account
+                portalUrl
+                nocContact
+                comments
+              }
+            }
+          }
+        }
+        '''
+        expected = {'providers':
+                        {'edges': [{'node': {'id': 'UHJvdmlkZXJOb2RlOjExMQ==', 'slug': 'provider1',
+                                                      'name': 'Provider 1', 'asn': 256.0, 'account': '12345',
+                                                      'portalUrl': 'https://www.nine.ch', 'nocContact': 'noc_contact',
+                                                      'comments': 'comments'}}]}}
+        result = schema.execute(query)
+        assert not result.errors
+        assert result.data == expected
+
+    def test_update_provider(self):
+        initialize_circuits()
+        query = '''
+        mutation {
+          updateProvider(input: {id:"UHJvdmlkZXJOb2RlOjExMQ==", name: "Provider1", slug: "provider1231",
+          asn: 512, account: "account", portalUrl: "http://github.com/", nocContact:"noc", comments: "my comment"}) {
+            provider {
+              id
+              slug
+              name
+              asn
+              account
+              portalUrl
+              nocContact
+              comments
+            }
+          }
+        }
+        '''
+        expected = {'updateProvider':
+                        {'provider': {'id': 'UHJvdmlkZXJOb2RlOjExMQ==', 'slug': 'provider1231',
+                                      'name': 'Provider1', 'asn': 512.0, 'account': 'account',
+                                      'portalUrl': 'http://github.com/', 'nocContact': 'noc', 'comments': 'my comment'}}}
+
+        result = schema.execute(query)
+        assert not result.errors
+        assert result.data == expected
+
+    def test_delete_provider(self):
+        initialize_circuits()
+        query = '''
+        mutation {
+          deleteProvider(input: {id:"UHJvdmlkZXJOb2RlOjExMQ=="}) {
+            provider {
+              id
+              slug
+              name
+              asn
+              account
+              portalUrl
+              nocContact
+              comments
+            }
+          }
+        }
+        '''
+        expected = {'deleteProvider': {'provider': {'id': 'UHJvdmlkZXJOb2RlOk5vbmU=', 'slug': 'provider1',
+                                                    'name': 'Provider 1', 'asn': 256.0, 'account': '12345',
+                                                    'portalUrl': 'https://www.nine.ch', 'nocContact': 'noc_contact',
+                                                    'comments': 'comments'}}}
         result = schema.execute(query)
         assert not result.errors
         assert result.data == expected
