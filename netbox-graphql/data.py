307ee69bb39490741b6f16840af99c63253882be
misazr <miodrag.seslija@consulteer.com>
2017-09-06 14:45:30 +0200

    Added circuit unit tests for CRUD

--- netbox-graphql/data.py
+++ netbox-graphql/data.py
@@ -1,6 +1,7 @@
 from circuits.models import CircuitType, Circuit, Provider, CircuitTermination
+from dcim.models import Site, Interface
 
-def initialize_circuits():
+def initialize_circuit_type():
 
     circuit_type = CircuitType(
         id = '111',
@@ -8,7 +9,9 @@ def initialize_circuits():
         slug = 'type1'
     )
     circuit_type.save()
+    return  circuit_type
 
+def initialize_provider():
     provider = Provider(
         id = '111',
         name = 'Provider 1',
@@ -22,3 +25,39 @@ def initialize_circuits():
         created = '2015-01-15'
     )
     provider.save()
+    return provider
+
+def initialize_circuit(id):
+    circuit_type = CircuitType(
+        id = id,
+        name = 'Type' + id,
+        slug = 'type' + id
+    )
+    circuit_type.save()
+
+    provider = Provider(
+        id = id,
+        name = 'Provider'+id,
+        slug = 'provider'+id,
+        asn = '256',
+        account = '12345',
+        portal_url = 'https://www.nine.ch',
+        noc_contact = 'noc_contact',
+        admin_contact = 'admin_contact',
+        comments = 'comments',
+        created = '2015-01-15'
+    )
+    provider.save()
+
+    circuit = Circuit(
+        id = id,
+        cid = 'cid',
+        provider = provider,
+        type = circuit_type,
+        install_date = '2017-10-12',
+        commit_rate = 12,
+        description = 'desc',
+        comments = 'comments'
+    )
+    circuit.save()
+    return circuit
