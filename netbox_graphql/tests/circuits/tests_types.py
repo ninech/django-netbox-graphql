2514e7dbd05edd96a6bd9d0381d6a92a4c3725e1
thde <demian@nine.ch>
2018-02-06 09:43:16 +0100

    refactored tests

--- netbox_graphql/tests/circuits/tests_types.py
+++ netbox_graphql/tests/circuits/tests_types.py
@@ -1,18 +1,22 @@
+from string import Template
 
 from graphene.test import Client
-from snapshottest import TestCase
+from django.test import TestCase
 from netbox_graphql.tests.data import *
+from netbox_graphql.tests.utils import obj_to_global_id
 from netbox_graphql.schema import schema
+
+from graphql_relay.node.node import from_global_id, to_global_id
+
 from circuits.models import CircuitType, Circuit, Provider, CircuitTermination
 
 
-
-
-class CircuitTypeTestCase(TestCase):
-    def test_creating_new_circuit_type(self):
-        query = '''
+class CircuitTypeCreateTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.query = '''
         mutation {
-          newCircuitType(input: {name: "Type", slug: "type"}) {
+          newCircuitType(input: {name: "Typename", slug: "typeslug"}) {
             circuitType {
               name
               slug
@@ -20,71 +24,179 @@ class CircuitTypeTestCase(TestCase):
           }
         }
         '''
-        expected = {'newCircuitType':
-                    {'circuitType': {'name': 'Type', 'slug': 'type'}}}
 
-        result = schema.execute(query)
+    def test_creating_circuit_type_returns_no_error(self):
+        result = schema.execute(self.query)
         assert not result.errors
-        assert result.data == expected
 
-    def test_correct_fetch_of_circuit_type(self):
-        initialize_circuit_type()
-        query = '''
+    def test_creating_circuit_type_returns_data(self):
+        expected = {'newCircuitType':
+                    {'circuitType': {'name': 'Typename', 'slug': 'typeslug'}}}
+
+        result = schema.execute(self.query)
+        self.assertEquals(result.data, expected)
+
+    def test_creating_circut_type_creates_it(self):
+        oldCount = CircuitType.objects.all().count()
+        schema.execute(self.query)
+        self.assertEquals(CircuitType.objects.all().count(), oldCount + 1)
+
+
+class CircuitTypeQueryMultipleTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.typeOne = CircuitType(
+            id='1234',
+            name='Awesome Type',
+            slug='awsme1'
+        )
+        cls.typeOne.save()
+
+        cls.typeTwo = CircuitType(
+            id='1235',
+            name='Pooor Type',
+            slug='poo1'
+        )
+        cls.typeTwo.save()
+
+        cls.query = '''
+        query {circuitTypes {
+            edges {
+                node {
+                id
+                name
+                slug
+                }
+            }
+        }}
+        '''
+
+    def test_querying_all_types_returns_no_error(self):
+        result = schema.execute(self.query)
+        assert not result.errors
+
+    def test_querying_all_types_returns_two_results(self):
+        result = schema.execute(self.query)
+        self.assertEquals(len(result.data['circuitTypes']['edges']), 2)
+
+
+class CircuitTypeQuerySingleTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.typeOne = CircuitType(
+            id='1234',
+            name='Awesome Type',
+            slug='awsme1'
+        )
+        cls.typeOne.save()
+
+        cls.typeTwo = CircuitType(
+            id='1235',
+            name='Pooor Type',
+            slug='poo1'
+        )
+        cls.typeTwo.save()
+
+        cls.query = Template('''
         {
-          circuitTypes(id: "Q2lyY3VpdFR5cGVOb2RlOjExMQ==") {
+          circuitTypes(id: "$id") {
             edges {
               node {
-                id
                 name
                 slug
               }
             }
           }
         }
-        '''
-        expected = {'circuitTypes': {
-            'edges': [{'node': {'id': 'Q2lyY3VpdFR5cGVOb2RlOjExMQ==', 'name': 'Type 1', 'slug': 'type1'}}]}}
+        ''').substitute(id=obj_to_global_id(cls.typeOne))
 
-        result = schema.execute(query)
+    def test_querying_single_type_returns_no_error(self):
+        result = schema.execute(self.query)
         assert not result.errors
-        assert result.data == expected
 
-    def test_update_circuit_type(self):
-        initialize_circuit_type()
-        query = '''
+    def test_querying_single_type_returns_result(self):
+        result = schema.execute(self.query)
+        self.assertEquals(len(result.data['circuitTypes']['edges']), 1)
+
+    def test_querying_single_type_returns_expected_result(self):
+        result = schema.execute(self.query)
+        expected = {'circuitTypes':
+                    {'edges': [
+                        {'node': {'name': 'Awesome Type', 'slug': 'awsme1'}}
+                    ]}
+                    }
+        self.assertEquals(result.data, expected)
+
+
+class CircuitTypeUpdateTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.typeOne = CircuitType(
+            id='1234',
+            name='Awesome Type',
+            slug='awsme1'
+        )
+        cls.typeOne.save()
+
+        cls.query = Template('''
         mutation {
-          updateCircuitType(input: {id:"Q2lyY3VpdFR5cGVOb2RlOjExMQ==", name: "TypeX", slug: "typex"}) {
+          updateCircuitType(input: {id:"$id", name: "New Name", slug: "nsl1"}) {
             circuitType {
-             id
               name
               slug
             }
           }
         }
-        '''
+        ''').substitute(id=obj_to_global_id(cls.typeOne))
+
+    def test_updating_returns_no_error(self):
+        result = schema.execute(self.query)
+        assert not result.errors
+
+    def test_updating_doesnt_change_count(self):
+        oldCount = CircuitType.objects.all().count()
+        schema.execute(self.query)
+        self.assertEquals(CircuitType.objects.all().count(), oldCount)
+
+    def test_updating_returns_updated_data(self):
         expected = {'updateCircuitType':
-                    {'circuitType': {'id': 'Q2lyY3VpdFR5cGVOb2RlOjExMQ==', 'name': 'TypeX', 'slug': 'typex'}}}
+                    {'circuitType': {'name': 'New Name', 'slug': 'nsl1'}}}
+        result = schema.execute(self.query)
+        self.assertEquals(result.data, expected)
 
-        result = schema.execute(query)
-        assert not result.errors
-        assert result.data == expected
+    def test_updating_alters_data(self):
+        schema.execute(self.query)
+        circuit_type = CircuitType.objects.get(id=self.typeOne.id)
+        self.assertEquals(circuit_type.name, 'New Name')
+        self.assertEquals(circuit_type.slug, 'nsl1')
 
-    def test_delete_circuit_type(self):
-        initialize_circuit_type()
-        query = '''
+
+class CircuitTypeDeleteTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.typeOne = CircuitType(
+            id='1234',
+            name='Awesome Type',
+            slug='awsme1'
+        )
+        cls.typeOne.save()
+
+        cls.query = Template('''
         mutation {
-          deleteCircuitType(input: {id:"Q2lyY3VpdFR5cGVOb2RlOjExMQ=="}) {
+          deleteCircuitType(input: {id:"$id"}) {
             circuitType {
-              id
               name
               slug
             }
           }
         }
-        '''
-        expected = {'deleteCircuitType':
-                    {'circuitType': {'id': 'Q2lyY3VpdFR5cGVOb2RlOk5vbmU=', 'name': 'Type 1', 'slug': 'type1'}}}
+        ''').substitute(id=obj_to_global_id(cls.typeOne))
 
-        result = schema.execute(query)
+    def test_deleting_returns_no_error(self):
+        result = schema.execute(self.query)
         assert not result.errors
-        assert result.data == expected
+
+    def test_deleting_removes_a_type(self):
+        oldCount = CircuitType.objects.all().count()
+        schema.execute(self.query)
+        self.assertEquals(CircuitType.objects.all().count(), oldCount - 1)
