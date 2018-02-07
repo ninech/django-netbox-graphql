c79fdb7383c998490deab7cf7d762e413c6a8ac3
thde <demian@nine.ch>
2018-02-07 09:35:02 +0100

    refactor tests

--- netbox_graphql/tests/tenancy/tests_tenants.py
+++ netbox_graphql/tests/tenancy/tests_tenants.py
@@ -1,113 +1,184 @@
 
+from string import Template
+
 from graphene.test import Client
-from snapshottest import TestCase
-from netbox_graphql.tests.data import *
+from django.test import TestCase
+
+from tenancy.models import Tenant
+
 from netbox_graphql.schema import schema
-from circuits.models import CircuitType, Circuit, Provider, CircuitTermination
+
+from netbox_graphql.tests.data import *
+from netbox_graphql.tests.utils import obj_to_global_id
+from netbox_graphql.tests.factories.tenant_factories import TenantFactory, TenantGroupFactory
 
 
-
-
-class TenantTestCase(TestCase):
-    def test_creating_new_tenant(self):
-        initialize_tenant_group('11')
-        query = '''
-        mutation {
-            newTenant(input: {name: "Tenant 1", slug: "tenant-1", group: "VGVuYW50R3JvdXBOb2RlOjEx=", description: "desc", comments: "comments"}) {
-            tenant {
-            name
-            slug
-            group {
-              name
-            }
-            description
-            comments
-            }
-          }
-        }
-        '''
-        expected = {'newTenant': {'tenant': {'name': 'Tenant 1', 'slug': 'tenant-1',
-                                             'group': {'name': 'Tenant Group11'}, 'description': 'desc',
-                                             'comments': 'comments'}}}
-
-        result = schema.execute(query)
-        assert not result.errors
-        assert result.data == expected
-
-    def test_correct_fetch_of_provider(self):
-        initialize_tenant('12')
-        query = '''
-        {
-          tenants(id: "VGVuYW50Tm9kZToxMg=="){
-            edges {
-              node {
-                id
-                name
-                slug
-                group {
-                  id
+class CreateTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.group = TenantGroupFactory()
+        cls.query = Template('''
+            mutation {
+              newTenant(input: {name: "New Tenant", slug: "t1", group: "$groupId", description: "desc", comments: "comments"}) {
+                tenant {
+                    name
+                    group {
+                        name
+                    }
                 }
-                description
-                comments
               }
             }
-          }
-        }
-        '''
-        expected = {'tenants': {'edges': [{'node': {'id': 'VGVuYW50Tm9kZToxMg==', 'name': 'Tenant 12',
-                                                    'slug': 'tenant-12', 'group': {'id': 'VGVuYW50R3JvdXBOb2RlOjEy'},
-                                                    'description': 'desc', 'comments': 'comment'}}]}}
-        result = schema.execute(query)
-        assert not result.errors
-        assert result.data == expected
-    #
+            ''').substitute(groupId=obj_to_global_id(cls.group))
 
-    def test_update_provider(self):
-        initialize_tenant('14')
-        query = '''
-        mutation {
-          updateTenant(input: {id: "VGVuYW50Tm9kZTox", name: "Tenant A", slug: "tenant-A", group: "VGVuYW50R3JvdXBOb2RlOjE=", description: "descA", comments: "commentsA"}) {
-            tenant {
-              id
-              name
-              slug
-              group {
-                id
-              }
-              description
-              comments
+    def test_creating_returns_no_error(self):
+        result = schema.execute(self.query)
+        assert not result.errors
+
+    def test_creating_returns_data(self):
+        expected = {'newTenant':
+                    {'tenant': {'name': 'New Tenant',
+                                'group': {'name': self.group.name}}}}
+
+        result = schema.execute(self.query)
+        self.assertEquals(result.data, expected)
+
+    def test_creating_creates_it(self):
+        oldCount = Tenant.objects.all().count()
+        schema.execute(self.query)
+        self.assertEquals(Tenant.objects.all().count(), oldCount + 1)
+
+
+class QueryMultipleTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.first = TenantFactory()
+        cls.second = TenantFactory(group=cls.first.group)
+        cls.third = TenantFactory()
+        cls.query = Template('''
+        {
+            tenants{
+                edges {
+                    node {
+                        name
+                        group {
+                            name
+                        }
+                    }
+                }
             }
-          }
         }
-        '''
-        expected = {'updateTenant': {'tenant': {'id': 'VGVuYW50Tm9kZTox', 'name': 'Tenant A', 'slug': 'tenant-A',
-                                                'group': {'id': 'VGVuYW50R3JvdXBOb2RlOjE='}, 'description': 'descA',
-                                                'comments': 'commentsA'}}}
+        ''').substitute(id=obj_to_global_id(cls.first))
 
-        result = schema.execute(query)
+    def test_querying_all_types_returns_no_error(self):
+        result = schema.execute(self.query)
         assert not result.errors
-        assert result.data == expected
 
-    def test_delete_tenant(self):
-        initialize_tenant('13')
-        query = '''
+    def test_querying_all_types_returns_three_results(self):
+        result = schema.execute(self.query)
+        self.assertEquals(len(result.data['tenants']['edges']), 3)
+
+
+class QuerySingleTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.first = TenantFactory()
+        cls.second = TenantFactory(group=cls.first.group)
+        cls.query = Template('''
+        {
+            tenants(id: "$id") {
+                edges {
+                    node {
+                        name
+                        group {
+                            name
+                        }
+                    }
+                }
+            }
+        }
+        ''').substitute(id=obj_to_global_id(cls.first))
+
+    def test_querying_single_type_returns_no_error(self):
+        result = schema.execute(self.query)
+        assert not result.errors
+
+    def test_querying_single_type_returns_result(self):
+        result = schema.execute(self.query)
+        self.assertEquals(len(result.data['tenants']['edges']), 1)
+
+    def test_querying_single_type_returns_expected_result(self):
+        result = schema.execute(self.query)
+        expected = {'tenants':
+                    {'edges': [
+                        {'node': {'name': self.first.name,
+                                  'group': {'name': self.first.group.name}}}
+                    ]}}
+        self.assertEquals(result.data, expected)
+
+
+class UpdateTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.first = TenantFactory()
+        cls.group = TenantGroupFactory()
+        cls.query = Template('''
         mutation {
-          deleteTenant(input: {id: "VGVuYW50Tm9kZToxMg=="}) {
+          updateTenant(input: {id: "$id", name: "New Name", slug: "nsl1", group: "$groupId"}) {
             tenant {
               name
               slug
               group {
                 name
               }
-              description
-              comments
             }
           }
         }
-        '''
-        expected = {'deleteTenant': {
-            'tenant': {'name': 'Tenant 12', 'slug': 'tenant-12', 'group': {'name': 'Tenant Group12'},
-                       'description': 'desc', 'comments': 'comment'}}}
-        result = schema.execute(query)
+        ''').substitute(id=obj_to_global_id(cls.first), groupId=obj_to_global_id(cls.group))
+
+    def test_updating_returns_no_error(self):
+        result = schema.execute(self.query)
         assert not result.errors
-        assert result.data == expected
+
+    def test_updating_doesnt_change_count(self):
+        oldCount = Tenant.objects.all().count()
+        schema.execute(self.query)
+        self.assertEquals(Tenant.objects.all().count(), oldCount)
+
+    def test_updating_returns_updated_data(self):
+        expected = {'updateTenant':
+                    {'tenant': {'name': 'New Name',
+                                'slug': 'nsl1',
+                                'group': {'name': self.group.name}}}}
+        result = schema.execute(self.query)
+        self.assertEquals(result.data, expected)
+
+    def test_updating_alters_data(self):
+        schema.execute(self.query)
+        tenant = Tenant.objects.get(id=self.first.id)
+        self.assertEquals(tenant.name, 'New Name')
+        self.assertEquals(tenant.slug, 'nsl1')
+        self.assertEquals(tenant.group, self.group)
+
+
+class DeleteTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.first = TenantFactory()
+        cls.query = Template('''
+        mutation {
+          deleteTenant(input: {id: "$id"}) {
+            tenant {
+              name
+            }
+          }
+        }
+        ''').substitute(id=obj_to_global_id(cls.first))
+
+    def test_deleting_returns_no_error(self):
+        result = schema.execute(self.query)
+        assert not result.errors
+
+    def test_deleting_removes_a_type(self):
+        oldCount = Tenant.objects.all().count()
+        schema.execute(self.query)
+        self.assertEquals(Tenant.objects.all().count(), oldCount - 1)
